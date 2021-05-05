57ff43d441aa44679e77659010c8d164dac0b606
RgnDunes <singh.divyansh1802@gmail.com>
2021-05-06 02:29:29 +0530

    Added Sidebar styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feed.css";
+import Header from "./Header";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <Header />
+      {/* <TweetBox />
+      <Post /> */}
+    </div>
+  );
+}
+
+export default Feed;
